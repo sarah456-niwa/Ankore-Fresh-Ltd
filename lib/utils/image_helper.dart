9e72390d8fe9a98e6b3ac18ceebfd7776ada2e5c
Sarafina Niwahereza <snuwahereza0@gmail.com>
2026-03-03 16:57:15 +0300

    changes

---
+++ lib/utils/image_helper.dart
@@ -0,0 +1,42 @@
+class ImageHelper {
+//   // Category icons
+//   static const String categoryAll = 'assets/icons/category_all.png';
+//   static const String categoryFruits = 'assets/icons/category_fruits.png';
+//   static const String categoryVegetables = 'assets/icons/category_vegetables.png';
+//   static const String categoryJuice = 'assets/icons/category_juice.png';
+//   static const String categoryOrganic = 'assets/icons/category_organic.png';
+  
+  // Product images
+  static const String mangoes = 'assets/images/fruits/mangoes.jpg';
+  static const String passionFruits = 'assets/images/fruits/passion.jpg';
+  static const String pawpaws = 'assets/images/fruits/pawpaws.jpg';
+  static const String oranges = 'assets/images/fruits/oranges.jpg';
+  static const String avocados = 'assets/images/fruits/avocados.jpg';
+  static const String watermelons = 'assets/images/fruits/watermelons.jpg';
+  static const String pineapples = 'assets/images/fruits/pineapples.jpg';
+  static const String bananas = 'assets/images/fruits/bananas.jpg';
+  
+  static const String greenPeppers = 'assets/images/vegetables/green_peppers.jpg';
+  static const String tomatoes = 'assets/images/vegetables/tomatoes.jpg';
+  static const String eggplants = 'assets/images/vegetables/eggplants.jpg';
+  static const String spinach = 'assets/images/vegetables/spinach.jpg';
+  static const String cabbage = 'assets/images/vegetables/cabbage.jpg';
+  static const String carrots = 'assets/images/vegetables/carrots.jpg';
+  static const String sukumaWiki = 'assets/images/vegetables/sukuma.jpg';
+  static const String dodo = 'assets/images/vegetables/dodo.jpg';
+  static const String nakati = 'assets/images/vegetables/nakati.jpg';
+  static const String jobyo = 'assets/images/vegetables/jobyo.jpg';
+  static const String cassavaLeaves = 'assets/images/vegetables/cassava_leaves.jpg';
+  static const String pumpkinLeaves = 'assets/images/vegetables/pumpkin_leaves.jpg';
+  static const String onions = 'assets/images/vegetables/onions.jpg';
+  static const String irishPotatoes = 'assets/images/vegetables/irish_potatoes.jpg';
+  static const String sweetPotatoes = 'assets/images/vegetables/sweet_potatoes.jpg';
+  
+  static const String pineappleJuice = 'assets/images/juice/pineapple_juice.jpg';
+  static const String passionJuice = 'assets/images/juice/passion_juice.jpg';
+  static const String mixedJuice = 'assets/images/juice/mixed_juice.jpg';
+  static const String strawberryJuice = 'assets/images/juice/strawberry_juice.jpg';
+  
+  // Placeholder image for missing images
+  static const String placeholder = 'assets/images/placeholder.jpg';
+}
